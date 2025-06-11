361c933eb9cd32f630cb65c83b43af7788495af8
ayclqt <quangtri.lam.9@gmail.com>
2025-06-11 15:24:06 +0700

    thiếu file

---
+++ src/api/services/__init__.py
@@ -0,0 +1,15 @@
+"""
+Services module for the chatbot application.
+"""
+
+from .conversation_service import ConversationService
+
+__all__ = ["ConversationService"]
+
+__author__ = "Lâm Quang Trí"
+__copyright__ = "Copyright 2025, Lâm Quang Trí"
+__credits__ = ["Lâm Quang Trí"]
+
+__maintainer__ = "Lâm Quang Trí"
+__email__ = "quangtri.lam.9@gmail.com"
+__status__ = "Development"
